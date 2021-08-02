e5221712e8f828792134a28ca3a6d35c6e8fcd85
한재원 <hanjo8813@gmail.com>
2021-08-03 03:06:46 +0900

    W1D1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Programmers_Devcourse
+# 프로그래머스 데브코스
+
+### 과정 중 이것저것 테스트하는 곳
+
+<br>
+
+### 링크
+> 기록은 여기에서
+- [TIL 블로그](https://hanjo8813.github.io/categories/TIL)
---
+++ Week1/ide_test/src/main/java/com/programmers/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.programmers.java;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        do_some("Hello w a-b");
+    }
+
+    private static void do_some(String bye) {
+        System.out.println(bye);
+    }
+}
