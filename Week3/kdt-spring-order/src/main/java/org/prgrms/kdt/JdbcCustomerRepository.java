363f935e34958a4145319f1b99a4992661897b2b
한재원 <hanjo8813@gmail.com>
2021-08-25 03:41:24 +0900

    W4D2

---
+++ Week3/kdt-spring-order/src/main/java/org/prgrms/kdt/JdbcCustomerRepository.java
@@ -0,0 +1,171 @@
+package org.prgrms.kdt;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class JdbcCustomerRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
+    private final String SELECT_BY_NAME_SQL = "select * from customers where name = ?";
+    private final String SELECT_ALL_SQL = "select * from customers";
+    private final String INSERT_SQL = "insert into customers(customer_id, name, email) values (UUID_TO_BIN(?), ?, ?)";
+    private final String UPDATE_BY_ID_SQL = "update customers set name = ? where customer_id = UUID_TO_BIN(?)";
+    private final String DELETE_ALL_SQL = "delete from customers";
+
+
+    public List<String> findNames(String name) {
+        List<String> names = new ArrayList<>();
+        try (
+                var connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "root1234");
+                var statement = connection.prepareStatement(SELECT_BY_NAME_SQL);
+        ) {
+            statement.setString(1, name);
+            logger.info("statement -> {}", statement);
+
+            try (var resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    var customerName = resultSet.getString("name");
+                    var customerId = UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id"));
+                    var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+                    logger.info("customer id -> {} , name -> {} , createdAt -> {}", customerId, customerName, createdAt);
+                    names.add(customerName);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("sql 수행중 오류 발생", e);
+        }
+
+        return names;
+    }
+
+
+    public List<String> findAllName() {
+        List<String> names = new ArrayList<>();
+        try (
+                var connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "root1234");
+                var statement = connection.prepareStatement(SELECT_ALL_SQL);
+                var resultSet = statement.executeQuery()
+        ) {
+            while (resultSet.next()) {
+                var customerName = resultSet.getString("name");
+                var customerId = UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id"));
+                var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+                //logger.info("customer id -> {} , name -> {} , createdAt -> {}", customerId, customerName, createdAt);
+                names.add(customerName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("sql 수행중 오류 발생", e);
+        }
+        return names;
+    }
+
+
+    public List<UUID> findAllId() {
+        List<UUID> uuids = new ArrayList<>();
+        try (
+                var connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "root1234");
+                var statement = connection.prepareStatement(SELECT_ALL_SQL);
+                var resultSet = statement.executeQuery()
+        ) {
+            while (resultSet.next()) {
+                var customerId = toUUID(resultSet.getBytes("customer_id"));
+                uuids.add(customerId);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("sql 수행중 오류 발생", e);
+        }
+        return uuids;
+    }
+
+
+    public int insertCustomer(UUID customerId, String name, String email) {
+        try (
+                var connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "root1234");
+                var statement = connection.prepareStatement(INSERT_SQL);
+        ) {
+            statement.setBytes(1, customerId.toString().getBytes());
+            statement.setString(2, name);
+            statement.setString(3, email);
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("sql 수행중 오류 발생", e);
+        }
+        return 0;
+    }
+
+
+    public int updateCustomerName(UUID customerId, String name) {
+        try (
+                var connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "root1234");
+                var statement = connection.prepareStatement(UPDATE_BY_ID_SQL);
+        ) {
+            statement.setString(1, name);
+            statement.setBytes(2, customerId.toString().getBytes());
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("sql 수행중 오류 발생", e);
+        }
+        return 0;
+    }
+
+
+    public int deleteAllCustomer() {
+        try (
+                var connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "root1234");
+                var statement = connection.prepareStatement(DELETE_ALL_SQL);
+        ) {
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("sql 수행중 오류 발생", e);
+        }
+        return 0;
+    }
+
+
+    static UUID toUUID(byte[] bytes){
+        var byteBuffer = ByteBuffer.wrap(bytes);
+        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    }
+
+    public static void main(String[] args) throws SQLException {
+        var customerRepository = new JdbcCustomerRepository();
+
+        // 데이터 삭제
+        customerRepository.deleteAllCustomer();
+
+        // 데이터 삽입
+        var newCustomerId = UUID.randomUUID();
+        logger.info("새로 삽입되는 uuid -> {}", newCustomerId);
+        customerRepository.insertCustomer(newCustomerId, "new-user1", "new-user@mail.com");
+
+
+        customerRepository.findAllId().forEach(v -> logger.info("uuid -> {}", v));
+
+
+//        // 데이터 수정
+//        customerRepository.updateCustomerName(newCustomerId, "new-user1-updated");
+//
+//        // 테이블의 모든 데이터 조회
+//        customerRepository.findAllName();
+
+        // 입력한 이름 데이터 조회
+//        var names = customerRepository.findNames("tester01' or 'a'='a");
+//        names.forEach(v -> logger.info("Found name : {}", v));
+    }
+}
