7b77dfa1188ff12e93750752fa6dfdc1008dcc3a
한재원 <hanjo8813@gmail.com>
2021-10-03 03:11:21 +0900

    W8D5

--- Week8/jpa-practice/src/test/java/com/example/jpapractice/order/controller/OrderControllerTest.java
+++ Week8/jpa-practice/src/test/java/com/example/jpapractice/order/controller/OrderControllerTest.java
@@ -9,12 +9,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.time.LocalDateTime;
@@ -27,6 +31,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 @Slf4j
 @SpringBootTest
@@ -84,7 +89,6 @@ class OrderControllerTest {
 
     @Test
     void saveCallTest() throws Exception {
-
         // Given
         OrderDto orderDto = OrderDto.builder()
                 .uuid(UUID.randomUUID().toString())
@@ -120,7 +124,39 @@ class OrderControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(orderDto)))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document(
+                        "order_save",
+                        // 요청
+                        requestFields(
+                                fieldWithPath("uuid").type(JsonFieldType.STRING).description("UUID"),
+                                fieldWithPath("orderDatetime").type(JsonFieldType.STRING).description("orderDatetime"),
+                                fieldWithPath("orderStatus").type(JsonFieldType.STRING).description("orderStatus"),
+                                fieldWithPath("memo").type(JsonFieldType.STRING).description("memo"),
+                                fieldWithPath("memberDto").type(JsonFieldType.OBJECT).description("memberDto"),
+                                fieldWithPath("memberDto.id").type(JsonFieldType.NULL).description("memberDto.id"),
+                                fieldWithPath("memberDto.name").type(JsonFieldType.STRING).description("memberDto.name"),
+                                fieldWithPath("memberDto.nickName").type(JsonFieldType.STRING).description("memberDto.nickName"),
+                                fieldWithPath("memberDto.age").type(JsonFieldType.NUMBER).description("memberDto.age"),
+                                fieldWithPath("memberDto.address").type(JsonFieldType.STRING).description("memberDto.address"),
+                                fieldWithPath("memberDto.description").type(JsonFieldType.STRING).description("memberDto.desc"),
+                                fieldWithPath("orderItemDtos[]").type(JsonFieldType.ARRAY).description("memberDto.desc"),
+                                fieldWithPath("orderItemDtos[].id").type(JsonFieldType.NULL).description("orderItemDtos.id"),
+                                fieldWithPath("orderItemDtos[].price").type(JsonFieldType.NUMBER).description("orderItemDtos.price"),
+                                fieldWithPath("orderItemDtos[].quantity").type(JsonFieldType.NUMBER).description("orderItemDtos.quantity"),
+                                fieldWithPath("orderItemDtos[].itemDtos[]").type(JsonFieldType.ARRAY).description("orderItemDtos.itemDtos"),
+                                fieldWithPath("orderItemDtos[].itemDtos[].price").type(JsonFieldType.NUMBER).description("orderItemDtos.itemDtos"),
+                                fieldWithPath("orderItemDtos[].itemDtos[].stockQuantity").type(JsonFieldType.NUMBER).description("orderItemDtos.itemDtos"),
+                                fieldWithPath("orderItemDtos[].itemDtos[].type").type(JsonFieldType.STRING).description("orderItemDtos.itemDtos"),
+                                fieldWithPath("orderItemDtos[].itemDtos[].chef").type(JsonFieldType.STRING).description("orderItemDtos.itemDtos")
+                        ),
+                        // 응답
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태코드"),
+                                fieldWithPath("data").type(JsonFieldType.STRING).description("데이터"),
+                                fieldWithPath("serverDateTime").type(JsonFieldType.STRING).description("응답시간")
+                        )
+                ));
     }
 
     @Test
