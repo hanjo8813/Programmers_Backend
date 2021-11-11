be1e4547cec91336fbedd8bbc06a5dfc00df4cfc
한재원 <hanjo8813@gmail.com>
2021-11-12 02:40:01 +0900

    W12D1

--- Spring_security/ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configures/WebMvcConfigure.java
+++ Spring_security/ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configures/WebMvcConfigure.java
@@ -1,4 +1,14 @@
 package com.prgrms.devcourse.springsecuritymasterclass.configures;
 
-public class WebMvcConfigure {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfigure implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/me").setViewName("me");
+    }
 }
--- Spring_security/ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configures/WebSecurityConfigure.java
+++ Spring_security/ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configures/WebSecurityConfigure.java
@@ -1,6 +1,7 @@
 package com.prgrms.devcourse.springsecuritymasterclass.configures;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
 
     // 전역설정 처리를 하는 API
     @Override
@@ -22,16 +28,16 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                // 인증영역 : me 요청시 "USER", "ADMIN"이어야 한다.
-                // 인가없이 /me에 접근시 /login 페이지로 redirect
-                .antMatchers("/me").hasAnyRole("USER", "ADMIN")
-                // 익명영역 : 인가없이 /me에
-                .anyRequest().permitAll()
-                .and()
+                    // 인증영역 : me 요청시 "USER", "ADMIN"이어야 한다.
+                    // 인가없이 /me에 접근시 /login 페이지로 redirect
+                    .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                    // 익명영역 : 인가없이 /me에
+                    .anyRequest().permitAll()
+                    .and()
                 .formLogin()
-                .defaultSuccessUrl("/")
-                .permitAll()
-                .and()
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+                    .and()
         ;
     }
 }
