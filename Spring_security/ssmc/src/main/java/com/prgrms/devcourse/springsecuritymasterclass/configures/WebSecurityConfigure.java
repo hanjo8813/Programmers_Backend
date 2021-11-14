f333088340a667612321eeee1d08cd5b41bc5299
한재원 <hanjo8813@gmail.com>
2021-11-14 21:27:50 +0900

    W16D1

--- Spring_security/ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configures/WebSecurityConfigure.java
+++ Spring_security/ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configures/WebSecurityConfigure.java
@@ -44,27 +44,30 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    // 인증영역 : me 요청시 "USER", "ADMIN"이어야 한다.
-                    // 인가없이 /me에 접근시 /login 페이지로 redirect
-                    .antMatchers("/me").hasAnyRole("USER", "ADMIN")
-                    // 익명영역 : 인가없이 /me에
+                    .antMatchers("/me").hasAnyRole("USER", "ADMIN") // 인증영역 : me 요청시 "USER", "ADMIN"이어야 한다.
                     .anyRequest().permitAll()
                     .and()
                 .formLogin()
-                    .defaultSuccessUrl("/")         // 로그인 성공시 url
-                    .permitAll()                    // ???
+                    .defaultSuccessUrl("/")                             // 로그인 성공시 url
+                    .permitAll()                                        // ???
                     .and()
-                .rememberMe()
-                    .key("key")
-                    .tokenValiditySeconds(60*5)
-                    .rememberMeParameter("param")
-                    .rememberMeCookieName("cookie_name")
+                .rememberMe()                                           // 쿠키를 통한 자동 로그인
+                    .key("key")                                         //
+                    .tokenValiditySeconds(60*5)                         // expire 기간 지정
+                    .rememberMeParameter("param")                       // 화면 체크박스의 'name' 속성을 지정
+                    .rememberMeCookieName("cookie_name")                //
                     .and()
                 .logout()
-                    .logoutUrl("/logout")           // 로그아웃 요청 url
-                    .logoutSuccessUrl("/login")     // 로그아웃 성공시 리다이렉트 url
-                    .invalidateHttpSession(true)    // ???
-                    .clearAuthentication(true)      // ??
+                    .logoutUrl("/logout")                               // 로그아웃 요청 url (default)
+                    .logoutSuccessUrl("/login")                         // 로그아웃 성공시 리다이렉트 url
+                    .invalidateHttpSession(true)                        // 로그아웃시 세션 제거하기 (default)
+                    .clearAuthentication(true)                          // security context 비우기 (default)
+                    .and()
+                .anonymous()
+                    .principal("익명유저")                                // 익명유저 name 지정
+                    .authorities("ROLE_ANONYMOUS", "ROLE_UNKNOWN")      // 익명유저 권한 지정
         ;
     }
+
+
 }
